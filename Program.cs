66607e1f6dba4795153675947e00ebec124451d0
rechkin <detox@mail.ru>
2015-09-19 15:33:54 +0600

    Test message

--- Program.cs
+++ Program.cs
@@ -106,7 +106,7 @@ namespace cabinet.alitrack.ru
 
                         #region Send report
                         string report = cabinet.alitrack.ru.Properties.Resources.Template.Replace("{NEW}", newItems.ToString());
-                        SmtpClient client = new SmtpClient("localhost");
+                        SmtpClient client = new SmtpClient(cabinet.alitrack.ru.Properties.Settings.Default.SMTP);
                         MailAddress from = new MailAddress(cabinet.alitrack.ru.Properties.Settings.Default.EmailFrom);
                         MailAddress to = new MailAddress(cabinet.alitrack.ru.Properties.Settings.Default.EmailTo);
                         MailMessage message = new MailMessage(from, to);
@@ -120,7 +120,7 @@ namespace cabinet.alitrack.ru
             }
             catch(Exception e)
             {
-                SmtpClient client = new SmtpClient("localhost");
+                SmtpClient client = new SmtpClient(cabinet.alitrack.ru.Properties.Settings.Default.SMTP);
                 MailAddress from = new MailAddress(cabinet.alitrack.ru.Properties.Settings.Default.EmailFrom);
                 MailAddress to = new MailAddress(cabinet.alitrack.ru.Properties.Settings.Default.EmailTo);
                 MailMessage message = new MailMessage(from, to);
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -67,5 +67,14 @@ namespace cabinet.alitrack.ru.Properties {
                 return ((string)(this["EmailFrom"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string SMTP {
+            get {
+                return ((string)(this["SMTP"]));
+            }
+        }
     }
 }
